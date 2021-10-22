8a41b9a9deb60c38568d8fab9db731735f1b105d
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2021-10-22 16:02:00 -0500

    Collectstatic

--- StaBarbara/settings.py
+++ StaBarbara/settings.py
@@ -183,7 +183,7 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT =   os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 django_heroku.settings(locals())
 
