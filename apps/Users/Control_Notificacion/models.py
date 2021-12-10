64c786cbf183d415742fb16e69eebe006c7eabb0
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2021-12-09 22:34:04 -0600

    Se agregó un campo a notificaciones

--- apps/Users/Control_Notificacion/models.py
+++ apps/Users/Control_Notificacion/models.py
@@ -8,6 +8,8 @@ class Control_Notificacion(models.Model):
     mensaje = models.CharField(max_length=254)
     id_user_emisor = models.ForeignKey(User , related_name='user_emisor' , on_delete=CASCADE  )
     id_user_receptor = models.ForeignKey(User ,  related_name='user_receptor' ,on_delete=CASCADE)
+    estado = models.CharField(max_length=254)
+
 
     def __str__(self):
         return str(self.id)
