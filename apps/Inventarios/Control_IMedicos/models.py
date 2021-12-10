e90fc0dcfe312aa1b87429c81e3cd2fdc121d78c
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2021-12-09 18:48:28 -0600

    Se agregó un nuevo campo a insumos

--- apps/Inventarios/Control_IMedicos/models.py
+++ apps/Inventarios/Control_IMedicos/models.py
@@ -7,6 +7,8 @@ class Inventario_Insumos(models.Model):
     cantidad = models.IntegerField()
     lote = models.CharField(max_length=254 , null=True)
     presentacion = models.FloatField(default=0)
+    producto_total = models.FloatField()
+
 
     def __str__(self):
         return str(self.id)
