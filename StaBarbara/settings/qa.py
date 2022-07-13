590aed93cb3c96d40f9de9764947a305920bda16
Fernando Guerrero <devrrior@gmail.com>
2022-07-12 21:30:04 -0500

    feat(settings): add qa settings

---
+++ StaBarbara/settings/qa.py
@@ -0,0 +1,41 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["18.216.104.88:8000"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": "5432",
+    }
+}
+
+# Configuration CORS
+CORS_ALLOWED_ORIGINS = [
+    "18.216.104.88:8000",
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+)
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
