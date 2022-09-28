4eeb77d1317a936885a3829ef9827cd1c09a58db
rguzman141189 <rguzman@ids.upchiapas.edu.mx>
2022-09-28 12:19:41 -0500

    change Control_Login views class Login

--- apps/Users/Control_Login/views.py
+++ apps/Users/Control_Login/views.py
@@ -6,18 +6,19 @@ from rest_framework.authtoken.models import Token
 
 from apps.Users.Control_Login.api.serializers import UserTokenSerializer
 
+
 class UserToken(APIView):
-    def get(self, request , *args , **kwargs):
+    def get(self, request, *args, **kwargs):
         username = request.GET.get('username')
 
         try:
             user_token = Token.objects.get(
-                user = UserTokenSerializer().Meta.model.objects.filter(username = username).first()
+                user=UserTokenSerializer().Meta.model.objects.filter(username=username).first()
             )
 
-            return Response({'token': user_token.key })
+            return Response({'token': user_token.key})
         except:
-            return Response({'error': 'Credenciales enviadas incorrectas'}, status= status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Credenciales enviadas incorrectas'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Create your views here.
@@ -34,10 +35,17 @@ class Login(ObtainAuthToken):
             return Response({
                 'token': token.key,
                 'user': user_serializer.data,
-                'message': 'Inicio de sesión exitoso'
-            })
+                'message': 'Inicio de sesión exitoso',
+                'status': True
+            },
+                status=status.HTTP_200_OK)
         else:
-            return Response({'message':'Usuario o contraseña incorrectos.'} , status=status.HTTP_400_BAD_REQUEST)    
+            return Response({
+                'token': '',
+                'user': '',
+                'message': 'Usuario o contraseña incorrecto',
+                'status': False
+            }, status=status.HTTP_400_BAD_REQUEST)
     # def post(self, request, *args, **kwargs):
     #     login_serializer = self.serializer_class(data = request.data , context = {'request' : request})
 
@@ -46,7 +54,7 @@ class Login(ObtainAuthToken):
     #         if user:
     #             token , created = Token.objects.get_or_create(user = user)
     #             user_serializer = UserTokenSerializer(user)
-    #             if created: 
+    #             if created:
     #                 return Response({
     #                     'token': token.key,
     #                     'user': user_serializer.data,
@@ -56,7 +64,7 @@ class Login(ObtainAuthToken):
     #                 token.delete()
     #                 token , created = Token.objects.get_or_create(user = user)
     #                 user_serializer = UserTokenSerializer(user)
-    #                 if created: 
+    #                 if created:
     #                     return Response({
     #                         'token': token.key,
     #                         'user': user_serializer.data,
@@ -71,12 +79,12 @@ class Login(ObtainAuthToken):
 
 
 class Logout(APIView):
-    #El front deberá enviar el token para poder hacer el logout.
+    # El front deberá enviar el token para poder hacer el logout.
 
-    def get(self , request , *args , **kwars):
+    def get(self, request, *args, **kwars):
         try:
             token = request.GET.get('token')
-            token = Token.objects.filter(key = token).first()
+            token = Token.objects.filter(key=token).first()
             if token:
                 user = token.user
 
@@ -84,11 +92,10 @@ class Logout(APIView):
                 session_message = 'Sesión cerrada'
                 token_message = 'Token eliminado'
 
-                return Response({'token_message': token_message , 'session_message' : session_message}
-                                ,status= status.HTTP_200_OK)
+                return Response({'token_message': token_message, 'session_message': session_message}, status=status.HTTP_200_OK)
 
-            return Response({'error': 'No se ha encontrado un usuario con estas credenciales.'} ,
-                        status= status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'No se ha encontrado un usuario con estas credenciales.'},
+                            status=status.HTTP_400_BAD_REQUEST)
         except:
-            return Response({'error': 'No se ha encontrado token en la petición.'} ,
-                        status= status.HTTP_409_CONFLICT)
+            return Response({'error': 'No se ha encontrado token en la petición.'},
+                            status=status.HTTP_409_CONFLICT)
